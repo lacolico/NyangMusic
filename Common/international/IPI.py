e30e169ef7c063c929a76de3a4d1cf39a60dd3b9
iamGreedy <31617752+iamGreedy@users.noreply.github.com>
2019-09-18 00:12:39 +0900

    국제규격 업데이트 완료
    ISRC 파싱, 포메팅 완성
    IPI 파싱, 포메팅 완성

--- Common/international/IPI.py
+++ Common/international/IPI.py
@@ -1,29 +1,62 @@
-from typing import List
+from __future__ import annotations
+
+from typing import List, Tuple
+
 import enum
+import re
 
 
 class IPIFormat(enum.Enum):
-    SEP: str = "{header}-{id}-{check}"
-    NO_SEP: str = "{header}{id}{check}"
+    SEP: Tuple[str, re.Pattern] = (
+        "{header}-{id}-{check}",
+        re.compile(
+            r"(?P<header>[a-zA-Z0-9])-(?P<id>[0-9]{9})-(?P<check>[0-9])")
+    )
+    NO_SEP: str = (
+        "{header}{id}{check}",
+        re.compile(
+            r"(?P<header>[a-zA-Z0-9])(?P<id>[0-9]{9})(?P<check>[0-9])")
+    )
 
 
 class IPI:
-    def __init__(self, header: str, identification_number: List[int], check_digit: int):
+    def __init__(self, header: str, identification: List[int], check: int):
         self._header: str = header
-        self._identification_number: List[int] = identification_number
-        self._check_digit: int = check_digit
+        self._identification: List[int] = identification
+        self._check: int = check
 
     def __str__(self):
-        return self.format(IPIFormat.SEP)
+        return self.format(IPIFormat.NO_SEP)
 
     def format(self, f: IPIFormat) -> str:
-        return f.value.format(
-            header=self._header,
-            id="".join(map(str, self._identification_number)),
-            check=self._check_digit,
+        return f.value[0].format(
+            header=self.header,
+            id=self.identification,
+            check=self._check,
         )
+
+    @classmethod
+    def parse(cls, raw: str, fmt: IPIFormat = IPIFormat.NO_SEP) -> IPI:
+        d = fmt.value[1].search(raw).groupdict()
+        return cls(d["header"], list(map(int, list(d["id"]))), int(d["check"]))
+
+    @property
+    def header(self) -> str:
+        return self._header
+
+    @property
+    def identification(self) -> str:
+        return "".join(map(str, self._identification))
+
+    @property
+    def check(self) -> int:
+        return self._check
 
 
 if __name__ == '__main__':
-    ex = IPI("I", [0, 0, 1, 0, 6, 8, 1, 3, 0], 6)
-    print(ex)
+    tmp = IPI.parse("I0010681306")
+
+    print(tmp)
+    print(tmp.header)
+    print(tmp.identification)
+    print(tmp.check)
--- Common/international/ISRC.py
+++ Common/international/ISRC.py
@@ -1,12 +1,23 @@
-import enum
-from typing import List
+from __future__ import annotations
 
+from typing import List, Tuple
+
+import re
+import enum
 import pycountry
 
 
 class ISRCFormat(enum.Enum):
-    SEP: str = "{country}-{registrant}-{year}-{designation}"
-    NO_SEP: str = "{country}{registrant}{year}{designation}"
+    SEP: Tuple[str, re.Pattern] = (
+        "{country}-{registrant}-{year}-{designation}",
+        re.compile(
+            r"(?P<country>[a-zA-Z]{2})-(?P<registrant>[a-zA-Z0-9]{3})-(?P<year>[0-9]{2})-(?P<designation>[0-9]{5})"),
+    )
+    NO_SEP: Tuple[str, re.Pattern] = (
+        "{country}{registrant}{year}{designation}",
+        re.compile(
+            r"(?P<country>[a-zA-Z]{2})(?P<registrant>[a-zA-Z0-9]{3})(?P<year>[0-9]{2})(?P<designation>[0-9]{5})"),
+    )
 
 
 class ISRC:
@@ -17,18 +28,43 @@ class ISRC:
         self._designation: List[int] = designation
 
     def __str__(self):
-        return self.format(ISRCFormat.SEP)
+        return self.format(ISRCFormat.NO_SEP)
 
     def format(self, f: ISRCFormat) -> str:
-        return f.value.format(
-            country=self._country,
+        return f.value[0].format(
+            country=self._country.alpha_2,
             registrant=self._registrant,
-            year=self._year,
-            designation="".join(map(str, self._designation)),
+            year="{:02d}".format(self._year),
+            designation=self.designation,
         )
 
+    @classmethod
+    def parse(cls, raw: str, fmt: ISRCFormat = ISRCFormat.NO_SEP) -> ISRC:
+        d = fmt.value[1].search(raw).groupdict()
+        return cls(pycountry.countries.get(alpha_2=d["country"]), d["registrant"], int(d["year"]),
+                   list(map(int, list(d["designation"]))))
+
+    @property
+    def country(self):
+        return self._country
+
+    @property
+    def registrant(self) -> str:
+        return self._registrant
+
+    @property
+    def year(self) -> int:
+        return self._year
+
+    @property
+    def designation(self) -> str:
+        return "".join(map(str, self._designation))
+
+
 if __name__ == '__main__':
-    tmp = pycountry.countries.get(alpha_2="KR")
-    print(type(tmp))
-    help(tmp)
-    print(tmp)
+    res = ISRC.parse("KRA381002339")
+    print(res)
+    print(res.country)
+    print(res.registrant)
+    print(res.year)
+    print(res.designation)
